e4da5277da2192c067e006a4d5a0ab9e5e6cc901
blainekuhn <blainekuhn@comcast.net>
2018-02-12 14:40:35 -0800

    Ver 1.2 complete

--- file_generator.py
+++ file_generator.py
@@ -21,54 +21,45 @@ from random import getrandbits
 from random import random, randint
 from time import sleep
 
-
-def dir_creator(path, d, w, deep):
-  d_gen = d #will eventually = [a for a in range(1, d)]
-  w_gen = w
- # print("In 10 sconds %s MB of data will be generated on the file system." % (d*w))
- # sleep(10)
-  dir_dict = name_gen(d_gen, w_gen)
-  file_maker(path, dir_dict) #will change when path is introduced
-
-
-def file_maker(path, f_dict):
-  MB = 1024*1024 # 1MB
-  count = 0
-  p = ""
-  for f in f_dict:
-    p = os.path.join(path, f)
-    print("This is p %s" % p)
-    os.mkdir(p)
-    f_list = f_dict[f]
-    for file in f_list:
-      #print("this is file %s" % file)
-      with open(os.path.join(p,file), 'wb') as b:
-        b.write(os.urandom(MB))
-
-
-def name_gen(d_gen, w_gen): #define the names of the folders
+def file_maker(path, d_gen, w_gen, deep):
   import string, random
   dir_dict= {}
   file_list = []
   word = ""
   dirname = ""
   alphabet = string.ascii_letters[0:52] + string.digits #+ string.punctuation
-  for a in range(d_gen):
-    for x in random.sample(alphabet,random.randint(8, 15)):
-      dirname += x
-    for b in range(w_gen):
+  MB = 1024*1024 # 1MB
+
+  while (deep +1) > 0:
+    for a in range(d_gen):
       for x in random.sample(alphabet,random.randint(8, 15)):
-        word += x
-      file_list.insert(b,word)
-      word = ""
-    dir_dict[dirname] = file_list
-    file_list = []
-    dirname = ""
- 
-  return(dir_dict)
+        dirname += x
+      for b in range(w_gen):
+        for x in random.sample(alphabet,random.randint(8, 15)):
+          word += x
+        file_list.insert(b,word)
+        word = ""
+      dir_dict[dirname] = file_list
+      file_list = []
+      dirname = ""
+    for f in dir_dict:
+      p = os.path.join(path, f)
+      print("This is p %s" % p)
+      os.mkdir(p)
+      f_list = dir_dict[f]
+      for file in f_list:
+        #print("FILE %s" % file)
+        #print("this is file %s" % file)
+        current_file = os.path.join(p,file)
+        with open(current_file, 'wb') as b:
+          b.write(os.urandom(MB))
+#        dir_creator(p, d_gen, w_gen, deep)
+      if (deep+1) > 0:
+        file_maker(p, d_gen, w_gen, deep-1)
+    return(dir_dict)
 
 
-dir_creator("/Users/blainekuhn/Git/Python/md5_checker", 2, 10, 3)
+file_maker("/Users/blainekuhn/Git/Python/File-Generator", 2, 10, 3)
 
 
 
